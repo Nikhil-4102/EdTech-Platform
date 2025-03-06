e39e388a0b5e5bd790bffc15b96466bf9dcf8528
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-03-07 02:12:18 +0530

    handler function for profile , subsection ,a nd section done

---
+++ controllers/Profile.js
@@ -0,0 +1,95 @@
+
+const Profile = require("../models/Profile");
+const User  = require("../models/User");
+
+exports.updateProfile = async (req , res) =>{
+    try {
+        //get data
+        const {dateOfBirth="",about="",contactNumber , gender} = req.body;
+        //get userId
+        const id = req.user.id;
+        //validation
+        if(!contactNumber || !gender || !id){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required",
+            });
+        }
+        //find profile
+        const userDetails = await User.findById(id);
+        const profileId = userDetails.additionalDetails;
+        const profileDetails = await Profile.findById(profileId);
+        //update
+        profileDetails.dateOfBirth =dateOfBirth,
+        profileDetails.about = about,
+        profileDetails.gender = gender,
+        profileDetails.contactNumber = contactNumber;
+        await profileDetails.save();
+        //return responce
+        return res.status(200).json({
+            success:true,
+            message:"Profile Updated Successfully",
+            profileDetails,
+        });
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            error:error.message,
+        })
+    }
+
+
+};
+
+
+//delete account
+
+exports.deleteAccount = async (req,res) =>{
+    try {
+        
+        const id = req.user.id;
+        const userDetails = await User.findById(id);
+        if(!userDetails){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            });
+        }
+
+        await Profile.findByIdAndDelete({_id:userDetails.additionalDetails});
+
+        await User.findByIdAndDelete({_id:id});
+
+        return  res.status(200).json({
+            success:true,
+            message:"User deleted Successfully",
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            error:error.message,
+        })
+    }
+}
+
+exports.getAllUserDetails = async (req,res) =>{
+    try {
+        const id = req.user.id;
+        const userDetails = await User.findById(id).populate("additionalDetails").exec();
+
+        return res.status(200).json({
+            success:true,
+            message:"User Data fetched successfully",
+
+        })
+
+    } catch (error) {
+         return res.status(500).json({
+            success:false,
+            message:""
+         })
+    }
+}
--- controllers/Subsection.js
+++ controllers/Subsection.js
@@ -1,2 +1,49 @@
 const SubSection = require("../models/SubSection");
-const 
+const Section = require("../models/Section");
+
+exports.createSubSection = async (req,res) => {
+    try {
+        
+        //fetch data from req body
+        const {sectionId , title , timeDuration , description} = req.body ;
+        //extract file/video
+        const video = req.files.videoFile ;
+
+        //validation
+        if(!sectionId || !timeDuration || !title || !description ){
+            return res.status(400).json({
+                success:false,
+                message:"ALL filed are required",
+            });
+        }
+        //upload video to cloudinary
+        const uploadDetails = await uploadImageToCloudinary(video , process.env.FOLDER_NAME);
+        // create a sub-section
+        const subSectionDetails = await SubSection.create({
+            title:title,
+            timeDuration:timeDuration,
+            description:description,
+            videoUrl:uploadDetails.secure_url,
+        })
+        //update section with this sub section ObjectID
+        const updatedSection = await Section.findByIdAndUpdate({_id:sectionId},
+                                             {$push:{
+                                                subSection:SubSectionDetails._id,
+                                             }},
+                                             {new:true});
+        // return responce
+        return res.status(200).json({
+            success:true,
+            message:"Sub Section created Succesfully",
+            updatedSection,
+        });
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Inrteranl Server Error",
+            error:error.message,
+        })
+    }
+}
