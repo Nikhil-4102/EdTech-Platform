1e010f927f53e58fe1849667626e64594e596d92
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-03-10 22:02:01 +0530

    home section 2 done

--- Frontend/src/components/core/HomePage/HighlightText.jsx
+++ Frontend/src/components/core/HomePage/HighlightText.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function HighlightText({text}) {
+const HighlightText = ({text}) => {
   return (
     <span className='font-bold text-richblue-200'>
         {" "}
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,111 +1,141 @@
 import React from 'react'
-import {FaArrowRight} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {FaArrowRight} from "react-icons/fa"
+import {Link} from "react-router-dom"
 import HighlightText from '../components/core/HomePage/HighlightText'
 
-import CTAButton from '../components/core/HomePage/Button'
-import Banner from '../assets/Images/banner.mp4'
-import CodeBlocks from '../components/core/HomePage/CodeBlocks'
+import CTAButton from "../components/core/HomePage/Button"
+import Banner from "../assets/Images/banner.mp4"
+import CodeBlocks from "../components/core/HomePage/CodeBlocks"
 
 const Home = () => {
   return (
     <div>
-       {/* {Section 1} */}
-        <div className=' relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white justify-between '>
-            <   Link to ={"/signup"}>
+      {/*Section1  */}
+      <div className='relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center 
+      text-white justify-between'>
 
-               <div className='group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 
-                   transition-all duration-200 hover:scale-95 w-fit'>
-                    <div className='flex flex-row items-center gap-2 rounded-full px-8 py-[5px]
-                            transition-all duration-200 group-hover:bg-richblack-900'>
-                       <p>Become an Instructor</p>
-                       <FaArrowRight></FaArrowRight>
-                    </div>
-               </div>
-
-
-            </Link>
-
-            <div className='text-center text-4xl font-semibold mt-7'>
-                Empower Your Future with 
-                <HighlightText text={"Coding Skills"} />
+        <Link to={"/signup"}>
+            <div className=' group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200
+            transition-all duration-200 hover:scale-95 w-fit'>
+                <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px]
+                transition-all duration-200 group-hover:bg-richblack-900'>
+                    <p>Become an Instructor</p>
+                    <FaArrowRight />
+                </div>
             </div>
 
-            <div className='mt-4 w-[90%] text-center text-lg font-bold text-richblack-300'>
-                With our online coding courses, you can learn at your own pace, from anywhere in the world , and get access to a wealth of resources , including hands-on projects,quizzes,and personalized feedback from instructors.
-            </div>
+        </Link>
 
-            <div className='flex flex-row gap-7 mt-8'>
-                 <CTAButton active={true} linkto={"/signup"}>Learn More</CTAButton>
-                 <CTAButton active={false} linkto={"/login"}>Book a Demo</CTAButton>
-            </div>
-
-            <div className='mx-3 my-12 shadow -blue-200'>
-                <video muted autoPlay loop >
-                    <source src={Banner} type='video/mp4'/>
-                </video>
-            </div>
-
-            {/* {Code section A} */}
-            <div>
-                <CodeBlocks 
-                           position={"lg:flex-row"}
-                           heading = {
-                                 <div className='text-4xl font -semibold'>
-                                    Unlock Your
-                                    <HighlightText text={"Coding Potential"} />
-                                    with our onlne courses
-                                 </div>
-                           } 
-                            subheading = {
-                                 <div>
-                                      Our courses are designed to help you learn coding from scratch, 
-                                      and provide you with the tools you need to succeed in the tech industry.
-                                 </div>
-                            }
-
-                            ctabtn1 = {{
-                                active : true,
-                                linkto : "/signup",
-                                btntext : "try it yourself"
-                            }}
-
-                            ctabtn2 = {{
-                                active : false,
-                                linkto : "/login",
-                                btntext : "learn more"
-                            }}
-
-                            codeblock = {`<<!DOCTYPE html>\n <html> \n <head><title>Example</ \n 
-                                title><link rel="stylesheet" href="styles.css"> \n
-                                /head> \n 
-                                <body> \n
-                                h1>< a href="/">Header</a> \n
-                                </h1> \n
-                                <nav> <a href="one/">One</a> \n
-                                <a href="two/">Two</a> \n 
-                                /nav>`}
-
-                            codeColor={text-yellow-25}
-
-                />
-            </div>
+        <div className='text-center text-4xl font-semibold mt-7'>
+            Empower Your Future with
+            <HighlightText text={"Coding Skills"} />
         </div>
-        
 
-       {/* {Section 2} */}
+        <div className=' mt-4 w-[90%] text-center text-lg font-bold text-richblack-300'>
+            With our online coding courses, you can learn at your own pace, from anywhere in the world, and get access to a wealth of resources, including hands-on projects, quizzes, and personalized feedback from instructors. 
+        </div>
+
+        <div className='flex flex-row gap-7 mt-8'>
+            <CTAButton active={true} linkto={"/signup"}> 
+                Learn More
+            </CTAButton>
+
+            <CTAButton active={false} linkto={"/login"}> 
+                Book a Demo
+            </CTAButton>
+        </div>
+
+        <div className='mx-3 my-12 shadow-blue-200'>
+            <video
+            muted
+            loop
+            autoPlay
+            >
+            <source  src={Banner} type="video/mp4" />
+            </video>
+        </div>
+
+        {/* Code Section 1 */}
+        <div>
+            <CodeBlocks 
+                position={"lg:flex-row"}
+                heading={
+                    <div className='text-4xl font-semibold'>
+                        Unlock Your
+                        <HighlightText text={"coding potential"}/>
+                        with our online courses
+                    </div>
+                }
+                subheading = {
+                    "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+                }
+                ctabtn1={
+                    {
+                        btnText: "try it yourself",
+                        linkto: "/signup",
+                        active: true,
+                    }
+                }
+                ctabtn2={
+                    {
+                        btnText: "learn more",
+                        linkto: "/login",
+                        active: false,
+                    }
+                }
+
+                codeblock={`<<!DOCTYPE html>\n<html>\nhead><title>Example</title><linkrel="stylesheet"href="styles.css">\n/head>\n`}
+                codeColor={"text-yellow-25"}
+            />
+        </div>
+
+                {/* Code Section 2 */}
+        <div>
+            <CodeBlocks 
+                position={"lg:flex-row-reverse"}
+                heading={
+                    <div className='text-4xl font-semibold'>
+                        Unlock Your
+                        <HighlightText text={"coding potential"}/>
+                        with our online courses
+                    </div>
+                }
+                subheading = {
+                    "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+                }
+                ctabtn1={
+                    {
+                        btnText: "try it yourself",
+                        linkto: "/signup",
+                        active: true,
+                    }
+                }
+                ctabtn2={
+                    {
+                        btnText: "learn more",
+                        linkto: "/login",
+                        active: false,
+                    }
+                }
+
+                codeblock={`<<!DOCTYPE html>\n<html>\nhead><title>Example</title><linkrel="stylesheet"href="styles.css">\n/head>\n`}
+                codeColor={"text-yellow-25"}
+            />
+        </div>
+
+      </div>
+
+      {/*Section 2  */}
 
 
-       {/* {Section 3} */}
+      {/*Section 3 */}
 
 
-       {/* {footer } */}
-
+      {/*Footer */}
 
 
     </div>
   )
 }
 
-export default Home ;
-
+export default Home
